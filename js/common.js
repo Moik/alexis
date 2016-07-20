d4675cb32b0f20623c72ae50c395d08c718580df
Moik <turangr@gmail.com>
2016-07-20 21:32:46 +0300

    Work Process

---
+++ js/common.js
@@ -0,0 +1,70 @@
+$(function() {
+
+	$('.toggle-menu').click(function(e) {
+		var menu = ".main-menu ul";
+		$(this).toggleClass('on');
+		$(menu).slideToggle();
+		e.preventDefault();
+	});
+
+	$(window).resize(function(){
+		var wid = $(window).width();
+		if(wid > 991 && $('.main-menu ul').is(':hidden')) {
+			$('.main-menu ul').removeAttr('style');
+		}
+
+		if (wid < 1199) {
+			$('.focus-card').removeAttr('style').equalHeights();
+		}
+
+		if (wid > 991) {
+			$('.teammate-about > div').removeAttr('style').equalHeights();
+		}
+	});
+
+	$('.focus-card').equalHeights();
+
+	smoothScroll(1000, "easeInOutQuart");
+
+
+	//Bootstrap Scroll Spy
+	$('[data-spy="scroll"]').each(function () {
+		var $spy = $(this).scrollspy('refresh');
+	});
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	$(".video-content").click(vid_play_pause);
+
+});
+
+$(window).load(function(){
+
+	$('.teammate-about > div').equalHeights();
+
+});
+
+function smoothScroll(duration, easing) {
+	$('.main-menu ul a[href^="#"]').click(function(event) {
+		var target = $( $(this).attr('href') );
+
+		if(target.length) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, duration, easing);
+		}
+	});
+}
+
+function vid_play_pause() {
+	var myVideo = document.getElementById("awesomeness");
+	myVideo.volume = 0.25;
+	if (myVideo.paused) {
+		$(".controls-wrap").fadeOut("200");
+		myVideo.play();
+	} else {
+		$(".controls-wrap").fadeIn("100");
+		myVideo.pause();
+	}
+}
