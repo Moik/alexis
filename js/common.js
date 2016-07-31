85d5883d0927b84ca6dffc715eb1284db3e952bb
Moik <turangr@gmail.com>
2016-07-31 14:08:09 +0300

    v. 0.9

--- js/common.js
+++ js/common.js
@@ -17,46 +17,59 @@ $(function() {
     });
 
 	$(window).resize(function(){
+
 		var wid = $(window).width();
+
 		if(wid > 991 && $('.main-menu ul').is(':hidden')) {
 			$('.main-menu ul').removeAttr('style');
 		}
 
-		if (wid < 1199) {
-			$('.focus-card').removeAttr('style').equalHeights();
-		}
-
-		if (wid > 991) {
-			$('.teammate-about > div').removeAttr('style').equalHeights();
+		if (wid < 992) {
+			$(".teammate-about").hide();
+			$('.teammate-about .equalize').removeAttr('style');
 		}
 	});
 
 	$('.focus-card').equalHeights();
+	$('.why-item').equalHeights();
+
+	teamStuff();
 
 	smoothScroll(1000, "easeInOutQuart");
 
 
 	//Bootstrap Scroll Spy
-	$('[data-spy="scroll"]').each(function () {
-		var $spy = $(this).scrollspy('refresh');
+	$('[data-spy="scroll"]').each(function() {
+		var $spy = $(this).scrollspy({
+			offset: 80
+		});
 	});
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
 	$(".video-content").click(vid_play_pause);
 
-	// $(window).scroll(function() {
-	// 	var wScroll = $(this).scrollTop();
-	// 	$('.header-descr').css({
-	// 		"transform" : "translate(0px, " + wScroll/25 + "%)"
-	// 	});
-	// });
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		items: 1,
+		dots: true,
+		autoplay: true,
+		autoplayHoverPause: true,
+		autoplaySpeed: 1000,
+		smartSpeed: 500 
+	});
 
 });
 
 $(window).load(function(){
 
-	$('.teammate-about > div').equalHeights();
+	$('body').addClass('loaded');
+
+	if ($(window).width() > 991) {
+		$('.team-card:first, .teammate-about:first-child').addClass('active');
+		$('.teammate-about:first-child').show();
+		$('.teammate-about .equalize').equalHeights();
+	}
 
 });
 
@@ -67,7 +80,7 @@ function smoothScroll(duration, easing) {
 		if(target.length) {
 			event.preventDefault();
 			$('html, body').animate({
-				scrollTop: target.offset().top
+				scrollTop: target.offset().top - 75
 			}, duration, easing);
 		}
 	});
@@ -83,4 +96,22 @@ function vid_play_pause() {
 		$(".controls-wrap").fadeIn("100");
 		myVideo.pause();
 	}
+}
+
+function teamStuff() {
+	$('.team-card').click(function() {
+		$('.team-card, .teammate-about').removeClass('active');
+		$(this).addClass('active');
+		$(".teammate-about").hide();
+
+		var i = $(this).parent().index();
+		var teammate = ".teammate-about:eq(" + i + ")";
+		$(teammate).show().addClass("active");
+		if ($(window).width() > 991) {
+			$('.teammate-about .equalize').equalHeights();
+		}
+	});
+	$('.popup-close').click(function() {
+		$(this).closest(".teammate-about").hide();
+	});
 }
