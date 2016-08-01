5b5b5d15d851b6c7fda0b871be77718f9bc546c9
Moik <turangr@gmail.com>
2016-08-01 21:04:53 +0300

    added landing-animation

--- js/common.js
+++ js/common.js
@@ -59,11 +59,71 @@ $(function() {
 		smartSpeed: 500 
 	});
 
+	$(window).scroll(function() {
+		var wScroll = $(this).scrollTop();
+
+		if(wScroll > $('#about-us .sect-content').offset().top - ($(window).height() * 0.5)) {
+			$('#about-us .sect-content').addClass("on");
+		}
+
+		if(wScroll > $('#focus').offset().top - ($(window).height() * 0.5)) {
+			$('.focus-wrap .focus-card').each(function(i) {
+				setTimeout(function() {
+					$('.focus-wrap .focus-card').eq(i).addClass('on');
+				}, 150 * (i + 1));
+			});
+		}
+
+		if(wScroll > $('#portfolio').offset().top - ($(window).height() * 0.5)) {
+			$('#portfolio .portfolio-item').each(function(i) {
+				setTimeout(function() {
+					$('#portfolio .portfolio-item').eq(i).addClass('on');
+				}, 150 * (i + 1));
+			});
+		}
+
+		if(wScroll > $('#work-steps').offset().top - ($(window).height() * 0.5)) {
+			$('#work-steps .step-unit').each(function(i) {
+				setTimeout(function() {
+					$('#work-steps .step-unit').eq(i).addClass('on');
+				}, 350 * (i + 1));
+			});
+		}
+
+		if(wScroll > $('#pricing').offset().top - ($(window).height() * 0.4)) {
+			$('#pricing .pricing-unit.main-accent').addClass('on');
+		}
+
+		if(wScroll > $('#why-sect').offset().top - ($(window).height() * 0.5)) {
+			$('#why-sect .why-item').each(function(i) {
+				setTimeout(function() {
+					$('#why-sect .why-item').eq(i).addClass('on');
+				}, 200 * (i + 1));
+			});
+		}
+
+		if(wScroll > $('.facts').offset().top - ($(window).height() * 0.5)) {
+			$('.facts .fact-item').addClass('on');
+		}
+
+		if(wScroll > $('.subscribe').offset().top - ($(window).height() * 0.6)) {
+			$('.subscribe .input-wrap').addClass('on');
+		}
+	});
+
 });
 
 $(window).load(function(){
 
 	$('body').addClass('loaded');
+
+	setTimeout(function() {
+		$('.teaser-item').each(function(i) {
+			setTimeout(function() {
+				$('.teaser-item').eq(i).addClass('on');
+			}, 150 * (i + 1));
+		});
+	}, 500);
 
 	if ($(window).width() > 991) {
 		$('.team-card:first, .teammate-about:first-child').addClass('active');
